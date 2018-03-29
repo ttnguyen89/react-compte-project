5c71f9dab4031da22eb4dc0accc81bc223b1868a
truc <trung-truc.nguyen@dps-fr.com>
2018-03-29 17:54:06 +0200

    edit home page

---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+/*
+##################################################################
+#           Copyright Digital Product Similation 2018            #
+#                                                                #
+# author : Trung-Truc Nguyen  									  #
+# email  : trung-truc.nguyen@dps-fr.com                          #
+##################################################################
+*/
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoggedOutView = (props) => {
+    if(!props.currentUser) {
+        return(
+            <ul className='nav navbar-nav pull-xs-right'>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-link'>
+                        Home
+                    </Link>
+                </li>
+
+                <li className='nav-item'>
+                    <Link to='/login' className='nav-link'>
+                        Sign In
+                    </Link>
+                </li>
+
+                <li className='nav-link'>
+                    <Link to='/register' className='nav-link'>
+                        Sign up
+                    </Link>
+                </li>
+            </ul>
+        )
+    }
+    return null;
+}
+
+const LoggedInView = props => {
+    if (props.currentUser) {
+      return (
+        <ul className="nav navbar-nav pull-xs-right">
+  
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+  
+          <li className="nav-item">
+            <Link to="/editor" className="nav-link">
+              <i className="ion-compose"></i>&nbsp;New Post
+            </Link>
+          </li>
+  
+          <li className="nav-item">
+            <Link to="/settings" className="nav-link">
+              <i className="ion-gear-a"></i>&nbsp;Settings
+            </Link>
+          </li>
+  
+          <li className="nav-item">
+            <Link
+              to={`/@${props.currentUser.username}`}
+              className="nav-link">
+              <img src={props.currentUser.image} className="user-pic" alt={props.currentUser.username} />
+              {props.currentUser.username}
+            </Link>
+          </li>
+  
+        </ul>
+      );
+    }
+  
+    return null;
+  };
+  
+  class Header extends React.Component {
+    render() {
+      return (
+        <nav className="navbar navbar-light">
+          <div className="container">
+  
+            <Link to="/" className="navbar-brand">
+              {this.props.appName.toLowerCase()}
+            </Link>
+  
+            <LoggedOutView currentUser={this.props.currentUser} />
+  
+            <LoggedInView currentUser={this.props.currentUser} />
+          </div>
+        </nav>
+      );
+    }
+  }
+
+export default Header;
